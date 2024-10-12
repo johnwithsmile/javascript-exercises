3b56272a7e892637c15ab4520c6bb5234e2106da
johnwithsmile <john_with_smile@kakao.com>
2024-10-12 23:02:10 +0900

    javascript exercises 1, 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let result = "";
+  if (num < 0) {
+    return "ERROR";
+  } else if (num === 0) {
+    return "";
+  } else {
+    for (let index = 1; index <= num; index++) {
+      result = result.concat(string);
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
