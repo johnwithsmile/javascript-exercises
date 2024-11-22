73b7e13cbc9c8c8430fc5547674a61e301b2c7ed
johnwithsmile <john_with_smile@kakao.com>
2024-11-22 15:34:33 +0900

    testcode, reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  if (string.length === 0) {
+    return "";
+  }
 
+  let reversed = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+  return reversed;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
+// const reverseString = function (string) {
+//     let splittedString = string.split("");
+//     if (splittedString.length === 0) {
+//       return "";
+//     }
+//     return splittedString.reverse().join("");
+//   };
+
+//   // Do not edit below this line
+//   module.exports = reverseString;
