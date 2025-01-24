7a30c758c3675db8cebd2b3aab98130aa38c961c
johnwithsmile <john_with_smile@kakao.com>
2025-01-24 17:08:10 +0900

    temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const convertToCelsius = function() {
+// 화씨에서 섭씨는 (F-32)* (5/9) = C
+// 소숫점 자르기는 number.toFixed(1); 사용한다.
+const convertToCelsius = function (number) {
+  if (typeof number !== "number") {
+    throw new Error("Input must be a number");
+  }
+
+  const result = (number - 32) * (5 / 9);
+  return result === 0 ? 0 : result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+// 섭씨에서 화씨는 (C*(9/5)) + 32 = F
+const convertToFahrenheit = function (number) {
+  if (typeof number !== "number") {
+    throw new Error("Input must be a number");
+  }
+
+  const result = number * (9 / 5) + 32;
+  return result === 0 ? 0 : result.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
--- 07_tempConversion/tempConversion.spec.js
+++ 07_tempConversion/tempConversion.spec.js
@@ -1,25 +1,30 @@
-const {convertToCelsius, convertToFahrenheit} = require('./tempConversion')
+const { convertToCelsius, convertToFahrenheit } = require("./tempConversion");
 
-describe('convertToCelsius', () => {
-  test('works', () => {
+describe("convertToCelsius", () => {
+  test("works", () => {
     expect(convertToCelsius(32)).toEqual(0);
   });
-  test.skip('rounds to 1 decimal', () => {
-    expect(convertToCelsius(100)).toEqual(37.8);
-  });
-  test.skip('works with negatives', () => {
-    expect(convertToCelsius(-100)).toEqual(-73.3);
-  });
+  "rounds to 1 decimal",
+    () => {
+      expect(convertToCelsius(100)).toEqual(37.8);
+    };
+  "works with negatives",
+    () => {
+      expect(convertToCelsius(-100)).toEqual(-73.3);
+    };
 });
 
-describe('convertToFahrenheit', () => {
-  test.skip('works', () => {
-    expect(convertToFahrenheit(0)).toEqual(32);
-  });
-  test.skip('rounds to 1 decimal', () => {
-    expect(convertToFahrenheit(73.2)).toEqual(163.8);
-  });
-  test.skip('works with negatives', () => {
-    expect(convertToFahrenheit(-10)).toEqual(14);
-  });
+describe("convertToFahrenheit", () => {
+  "works",
+    () => {
+      expect(convertToFahrenheit(0)).toEqual(32);
+    };
+  "rounds to 1 decimal",
+    () => {
+      expect(convertToFahrenheit(73.2)).toEqual(163.8);
+    };
+  "works with negatives",
+    () => {
+      expect(convertToFahrenheit(-10)).toEqual(14);
+    };
 });
